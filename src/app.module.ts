7fcd5bfeb84ebb24e88774444517fc736111aee8
David Peralta <peraltadavidr@outlook.com>
2022-10-23 02:30:44 +0800

    add users testing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -9,17 +8,34 @@ import { CitizenService } from './citizen/citizen.service';
 import { CitizenModule } from './citizen/citizen.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { PlayersService } from './players/players.service';
+import { AuthController } from './auth/auth.controller';
+import { UsersController } from './users/users.controller';
+import { PlayersController } from './players/players.controller';
+import { AuthService } from './auth/auth.service';
+import { UsersService } from './users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [
-    UsersModule,
-    AuthModule,
-    ConfigModule.forRoot(),
     PrismaModule,
+    AuthModule,
+    UsersModule,
     PlayersModule,
     CitizenModule,
   ],
-  controllers: [AppController],
-  providers: [AppService, PlayersService, CitizenService],
+  controllers: [
+    AppController,
+    AuthController,
+    UsersController,
+    PlayersController,
+  ],
+  providers: [
+    AppService,
+    AuthService,
+    UsersService,
+    PlayersService,
+    CitizenService,
+    JwtService,
+  ],
 })
 export class AppModule {}
---
+++ src/users/users.controller.spec.ts
@@ -0,0 +1,23 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { JwtStrategy } from 'src/auth/strategies';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+
+describe('UsersController', () => {
+  let controller: UsersController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [PrismaModule],
+      controllers: [UsersController],
+      providers: [UsersService, JwtStrategy],
+    }).compile();
+
+    controller = module.get<UsersController>(UsersController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
