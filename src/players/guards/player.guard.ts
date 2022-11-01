14f867fa4b955c2ed7cc40d2f44a9534942202de
David Peralta <peraltadavidr@outlook.com>
2022-11-01 18:00:58 +0800

    change player guard user property name

--- src/players/guards/player.guard.ts
+++ src/players/guards/player.guard.ts
@@ -2,4 +2,10 @@ import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class PlayerGuard extends AuthGuard('player') {}
+export class PlayerGuard extends AuthGuard('player') {
+  constructor() {
+    super({
+      property: 'player',
+    });
+  }
+}
--- src/players/players.module.ts
+++ src/players/players.module.ts
@@ -5,17 +5,9 @@ import { PrismaModule } from 'src/prisma/prisma.module';
 import { CitizenModule } from 'src/citizen/citizen.module';
 import { UsersModule } from 'src/users/users.module';
 import { PlayerStrategy } from './strategies/player.strategy';
-import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [
-    PrismaModule,
-    CitizenModule,
-    UsersModule,
-    PassportModule.register({
-      property: 'player',
-    }),
-  ],
+  imports: [PrismaModule, CitizenModule, UsersModule],
   controllers: [PlayersController],
   providers: [PlayersService, PlayerStrategy],
 })
