6fcdbccc9f495632ae3435521d3e81a92cf1f859
David Peralta <peraltadavidr@outlook.com>
2022-11-02 00:07:37 +0800

    remove unnecessary files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
@@ -8,7 +6,5 @@ import { PlayersModule } from './players/players.module';
 
 @Module({
   imports: [UsersModule, AuthModule, PlayersModule, ConfigModule.forRoot()],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
