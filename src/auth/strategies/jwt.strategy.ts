25db097b20ad165778fb8587d3d8b86da8d819fd
David Peralta <peraltadavidr@outlook.com>
2022-11-01 18:00:25 +0800

    change jwt strat validate not found error

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@nestjs/common';
 import { ENV } from 'src/constants/ENV';
 import { JwtToken } from '../auth.interface';
 import { UsersService } from 'src/users/users.service';
-import { InvalidCredentialsException } from 'src/errors';
+import { RecordNotFoundException } from 'src/errors';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
@@ -19,7 +19,9 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   async validate(token: JwtToken) {
     const user = await this.userService.findOne({ id: token.sub });
 
-    if (!user) throw new InvalidCredentialsException();
+    console.log(user);
+
+    if (!user) throw new RecordNotFoundException({ model: 'User' });
 
     return user;
   }
